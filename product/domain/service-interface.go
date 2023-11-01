85d25fb74a77e4db326e8bc931387194e78a8ebc
bright98 <www.hhsshhss@gmail.com>
2023-11-01 14:28:25 +0330

    fix: .gitignore and clean the git cache

--- product/domain/service-interface.go
+++
@@ -1,8 +0,0 @@
-package domain
-
-type ServiceInterface interface {
-	InsertProductService(product *Products) (string, *Errors)
-	UpdateProductService(id string, productAttr *ProductAttributes) *Errors
-	GetProductByIDService(id string) (*Products, *Errors)
-	GetAllProductsService(skip, limit int64) ([]Products, *Errors)
-}
--- revision/domain/service-interface.go
+++
@@ -1,7 +0,0 @@
-package domain
-
-type ServiceInterface interface {
-	InsertRevisionService(revision *Revisions) (string, *Errors)
-	GetRevisionByProductIDAndNoService(productID, revisionNo string) (*Products, *Errors)
-	GetAllRevisionsOfOneProductService(skip, limit int64, productID string) ([]Revisions, *Errors)
-}
